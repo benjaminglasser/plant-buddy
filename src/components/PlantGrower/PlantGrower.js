69fd9028345989d5cc3ce0cbbfd7437f069c9a0e
ben glasser <ben@fantasticvoyage.com>
2021-06-08 15:05:34 -0700

    fixes stem issue

--- src/components/PlantGrower/PlantGrower.js
+++ src/components/PlantGrower/PlantGrower.js
@@ -8,40 +8,42 @@ const PlantGrower = (props) => {
 
 
 
+
     return (
         <>
-            <div className="wrapper">
-                <div className="box">
-                    {props.leaf.buddies ? (
-                        <div className="plant">
-                            <div className="flower"></div>
-                            <div className="stem">
-                                {props.leaf.buddies.map((plant, idx) =>
-                                    <div key={idx}>
+            {props.leaf.buddies.length ? (
+                <div className="wrapper">
+                    <div className="box">
+                        {props.leaf.buddies ? (
+                            <div className="plant">
+                                <div className="flower"></div>
+                                <div className="stem">
+                                    {props.leaf.buddies.map((plant, idx) =>
+                                        <div key={idx}>
 
-                                        <ShowLeaf2
-                                            created={plant.updatedAt}
-                                            name={plant.name}
-                                            img={plant.img}
-                                            schedule={plant.schedule}
-                                            plantId={plant._id}
-                                            handleDelete={props.handleDelete}
-                                            idx={idx}
-                                            handleUpdate={props.handleUpdate}
-                                            handleEdit={props.handleEdit}
-                                        />
+                                            <ShowLeaf2
+                                                created={plant.updatedAt}
+                                                name={plant.name}
+                                                img={plant.img}
+                                                schedule={plant.schedule}
+                                                plantId={plant._id}
+                                                handleDelete={props.handleDelete}
+                                                idx={idx}
+                                                handleUpdate={props.handleUpdate}
+                                                handleEdit={props.handleEdit}
 
-                                    </div>
-                                )}
+                                            />
+                                        </div>
+                                    )}
+                                </div>
                             </div>
-                        </div>
-                    ) : window.location.reload()}
+                        ) : window.location.reload()}
 
-                    <div className="pot"></div>
-                    <div className="pot-top"></div>
+                        <div className="pot"></div>
+                        <div className="pot-top"></div>
+                    </div>
                 </div>
-            </div>
-
+            ) : <h1 className="no-plants">ADD A NEW PLANT</h1>}
         </>
     )
 }
